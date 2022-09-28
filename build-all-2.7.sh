8755f9b100d764dc5662d2aeb9d1e65802c2c9fc
Andrew McDermott <aim@frobware.com>
2022-09-28 10:50:18 +0100

    Add 2.7 build script

---
+++ build-all-2.7.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+set -eu
+
+thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+
+git remote update
+
+for i in $(git tag -l | grep v2.7 | sort -n); do
+    git checkout -f $i
+    git clean -f -d -x
+    echo  $i
+    ${thisdir}/../haproxy-openshift/build-haproxy-2.2.sh clean all
+    mv haproxy /tmp/haproxy-${i#v}
+done
