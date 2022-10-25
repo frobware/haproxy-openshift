899c125f22ef0a313486c0690bab3cf18392ba8b
Andrew McDermott <aim@frobware.com>
2022-10-25 14:44:19 +0100

    mb limit clients to matching backends

--- perf/mb.go
+++ perf/mb.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path"
 )
 
 type Request struct {
@@ -54,47 +55,49 @@ func filterInTrafficByType(types []TrafficType, backendsMap BoundBackendsByTraff
 }
 
 func (c *GenWorkloadCmd) Run(p *ProgramCtx) error {
-	backends, err := fetchAllBackendMetadata(p.DiscoveryURL)
+	if err := os.RemoveAll(path.Join(p.OutputDir, "mb")); err != nil {
+		return err
+	}
+
+	backendsByTrafficType, err := fetchAllBackendMetadata(p.DiscoveryURL)
 	if err != nil {
 		return err
 	}
 
-	for _, clients := range []int64{1, 10, 50, 100, 200} {
-		for _, scenario := range []struct {
-			Name         string
-			TrafficTypes []TrafficType
-		}{
-			{"edge", []TrafficType{EdgeTraffic}},
-			{"http", []TrafficType{HTTPTraffic}},
-			{"mix", AllTrafficTypes[:]},
-			{"passthrough", []TrafficType{PassthroughTraffic}},
-			{"reencrypt", []TrafficType{ReencryptTraffic}},
-		} {
-			for _, keepAliveRequests := range []int64{0} {
-				config := RequestConfig{
-					Clients:           clients,
-					KeepAliveRequests: keepAliveRequests,
-					TLSSessionReuse:   c.TLSReuse,
-					TrafficTypes:      scenario.TrafficTypes,
-				}
-				requests := generateWorkloadRequests(config, filterInTrafficByType(scenario.TrafficTypes, backends))
-				data, err := json.MarshalIndent(requests, "", "  ")
-				if err != nil {
-					return err
-				}
-				path := fmt.Sprintf("%s/mb/traffic-%v-backends-%v-clients-%v-keepalives-%v",
-					p.OutputDir,
-					scenario.Name,
-					len(requests)/len(config.TrafficTypes),
-					config.Clients,
-					config.KeepAliveRequests)
-				if err := os.MkdirAll(path, 0755); err != nil {
-					return fmt.Errorf("failed to create path: %q: %v", path, err)
-				}
-				filename := fmt.Sprintf("%s/requests.json", path)
-				if err := createFile(filename, data); err != nil {
-					return fmt.Errorf("error generating %s: %v", filename, err)
-				}
+	for _, scenario := range []struct {
+		Name         string
+		TrafficTypes []TrafficType
+	}{
+		{"edge", []TrafficType{EdgeTraffic}},
+		{"http", []TrafficType{HTTPTraffic}},
+		{"mix", AllTrafficTypes[:]},
+		{"passthrough", []TrafficType{PassthroughTraffic}},
+		{"reencrypt", []TrafficType{ReencryptTraffic}},
+	} {
+		for _, keepAliveRequests := range []int64{0} {
+			config := RequestConfig{
+				Clients:           int64(len(backendsByTrafficType[EdgeTraffic])),
+				KeepAliveRequests: keepAliveRequests,
+				TLSSessionReuse:   c.TLSReuse,
+				TrafficTypes:      scenario.TrafficTypes,
+			}
+			requests := generateWorkloadRequests(config, filterInTrafficByType(scenario.TrafficTypes, backendsByTrafficType))
+			data, err := json.MarshalIndent(requests, "", "  ")
+			if err != nil {
+				return err
+			}
+			path := fmt.Sprintf("%s/mb/traffic-%v-backends-%v-clients-%v-keepalives-%v",
+				p.OutputDir,
+				scenario.Name,
+				len(requests)/len(config.TrafficTypes),
+				config.Clients,
+				config.KeepAliveRequests)
+			if err := os.MkdirAll(path, 0755); err != nil {
+				return fmt.Errorf("failed to create path: %q: %v", path, err)
+			}
+			filename := fmt.Sprintf("%s/requests.json", path)
+			if err := createFile(filename, data); err != nil {
+				return fmt.Errorf("error generating %s: %v", filename, err)
 			}
 		}
 	}
