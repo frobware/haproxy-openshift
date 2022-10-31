9cf00e94903cbe3df7a3c0ef0ce7b184255d6f36
Andrew McDermott <aim@frobware.com>
2022-10-31 20:05:51 +0000

    simplify gen hosts

--- perf/hosts.go
+++ perf/hosts.go
@@ -9,22 +9,11 @@ func (c *GenHostsCmd) Run(p *ProgramCtx) error {
 	if c.IPAddress != "" {
 		addr = c.IPAddress
 	}
-	for _, t := range etcHosts(p, addr) {
-		fmt.Println(t)
-	}
-
-	return nil
-}
-
-func etcHosts(p *ProgramCtx, ipAddr string) []string {
-	var names []string
-
 	for _, t := range AllTrafficTypes {
 		for i := 0; i < p.Nbackends; i++ {
 			hostname := fmt.Sprintf("%v-%v-%v", p.HostPrefix, t, i)
-			names = append(names, fmt.Sprintf("%v %v", ipAddr, hostname))
+			fmt.Println(addr, hostname)
 		}
 	}
-
-	return names
+	return nil
 }
