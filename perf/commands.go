96effe9a93280630f6a5095c058d4e91a4808ee9
Andrew McDermott <aim@frobware.com>
2022-10-31 20:03:02 +0000

    move all CLI stuff into commands.go

--- perf/commands.go
+++ perf/commands.go
@@ -1,5 +1,36 @@
 package main
 
+import "context"
+
+type Globals struct {
+	Debug        bool        `help:"Enable debug mode" short:"D" default:"false"`
+	DiscoveryURL string      `help:"Backend metadata discovery URL" short:"u" default:"http://localhost:2000"`
+	HTTPPort     int         `help:"HAProxy HTTP port" default:"8080"`
+	HTTPSPort    int         `help:"HAProxy HTTPS port" default:"8443"`
+	HostPrefix   string      `help:"Hostname prefix" default:"perf-test-hydra"`
+	Nbackends    int         `help:"Number of backends per traffic type" short:"n" default:"1"`
+	OutputDir    string      `help:"Configuration output directory" short:"o" default:"testrun"`
+	Port         int         `help:"Port number for backend metadata server" short:"p" default:"2000"`
+	TLSReuse     bool        `help:"Enable TLS session reuse" default:"true"`
+	Version      VersionFlag `help:"Print version information and quit."`
+}
+
+type CLI struct {
+	Globals
+
+	GenProxyConfig GenProxyConfigCmd `cmd:"" help:"Generate HAProxy configuration."`
+	GenHosts       GenHostsCmd       `cmd:"" help:"Generate host names (/etc/hosts compatible)."`
+	GenWorkload    GenWorkloadCmd    `cmd:"" help:"Generate https://github.com/jmencak/mb requests."`
+	ServeBackend   ServeBackendCmd   `cmd:"" help:"Serve backend." hidden:"true"`
+	ServeBackends  ServeBackendsCmd  `cmd:"" help:"Serve backends."`
+	Version        VersionCmd        `cmd:"" help:"Print version information and quit."`
+}
+
+type ProgramCtx struct {
+	Context context.Context
+	Globals
+}
+
 type GenProxyConfigCmd struct {
 	ListenAddress        string `default:"::"`
 	Maxconn              int    `default:"0"`
