05eedc2024bc495f4554e834cdd56d62ba357086
Andrew McDermott <aim@frobware.com>
2023-02-02 10:02:10 +0000

    perf/test-haproxy: add parameter for traffic types to test

--- perf/test-haproxy.sh
+++ perf/test-haproxy.sh
@@ -7,6 +7,7 @@ trap "echo interrupt; exit 1" INT TERM
 : "${PROXY_HOST:?not set}"
 : "${DURATION:=60}"
 : "${MB:=$HOME/src/github.com/jmencak/mb/mb}"
+: "${TRAFFIC_TYPES:="edge http reencrypt passthrough"}"
 
 s1=1
 s2=8
@@ -17,13 +18,13 @@ metadata_dir="${top_level_results_dir}/$PROXY_HOST/.metadata"
 
 mkdir -p "${metadata_dir}"
 
+# Gather metadata
 ssh "$PROXY_HOST" haproxy -vv > "$metadata_dir/haproxy"
 ssh "$PROXY_HOST" sudo sysctl -a > "$metadata_dir/sysctl"
 ssh "$PROXY_HOST" cat /etc/os-release > "$metadata_dir/os-release"
-
 $MB version --version > "$metadata_dir/mb"
 
-for traffic_type in edge http reencrypt passthrough; do
+for traffic_type in ${TRAFFIC_TYPES}; do
     test_output_dir="${top_level_results_dir}/$PROXY_HOST/$traffic_type"
     mkdir -p "${test_output_dir}"
     for i in $(seq $s1 $s2); do
@@ -38,7 +39,7 @@ for traffic_type in edge http reencrypt passthrough; do
 	done
 	[[ $time_wait -gt 0 ]] && echo
 	echo "$MB --duration ${DURATION} --request-file ./testrun/requests/haproxy/traffic-${traffic_type}-backends-100-clients-100-keepalives-0.json > $stdout 2> $stderr"
-	$MB --duration ${DURATION} --request-file ./testrun/requests/haproxy/traffic-${traffic_type}-backends-100-clients-100-keepalives-0.json > "$stdout" 2> "$stderr"
+	$MB --duration ${DURATION} --request-file "./testrun/requests/haproxy/traffic-${traffic_type}-backends-100-clients-100-keepalives-0.json" > "$stdout" 2> "$stderr"
     done
     chmod -R u-w,g-w "${test_output_dir}"
 done
